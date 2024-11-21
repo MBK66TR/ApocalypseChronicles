33320a78dd8a43dd6e9039a0fe27d73be63db436
Mustafa Bugra <mustafakaraturk66@gmail.com>
2024-11-21 09:23:10 +0300

    Project Start

---
+++ Source/ApocalypseChronicles/ApocalypseChroniclesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ApocalypseChroniclesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APOCALYPSECHRONICLES_API AApocalypseChroniclesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
